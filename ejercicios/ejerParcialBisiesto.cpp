702a0f9e2a16b2575b9c932fc7b94b1dc83a7e91
Frackk <franco.abianxpg@gmail.com>
2026-04-30 21:45:39 -0300

    ejer bisiesto unf.

---
+++ ejercicios/ejerParcialBisiesto.cpp
@@ -0,0 +1,17 @@
+/*desarrolle una funcion que determine si un año es bisiesto.
+(Nota: es bisiesto cuando es divisible entre 4, excepto si tambien es divisible entre 100, a menos que tambien sea divisible entre 400)*/
+
+#include <iostream>
+using namespace std;
+
+int esBisiesto()
+
+int main () {
+
+
+
+
+
+
+    return 0;
+}
