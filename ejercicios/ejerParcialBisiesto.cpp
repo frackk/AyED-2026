1dcc2de0947815104aebf255f3373a100532c6ae
frack <franco.abianxpg@gmail.com>
2026-05-08 05:26:29 -0300

    ejerParcBisiesto

--- ejercicios/ejerParcialBisiesto.cpp
+++ ejercicios/ejerParcialBisiesto.cpp
@@ -4,14 +4,37 @@
 #include <iostream>
 using namespace std;
 
-//int esBisiesto()
+int esBisiesto(int anio);
 
 int main () {
 
+    int anioBis;
     
-    
+    cout << "ingrese un anio para analizar si es bisiesto: ";
+    cin >> anioBis; 
 
-
+    esBisiesto(anioBis);
 
     return 0;
 }
+
+int esBisiesto(int anio) {
+    if(anio % 4 == 0) {
+        if(anio % 100 == 0) {
+            if (anio % 400 == 0) {
+
+                cout << "el anio " << anio << " es bisiesto.";
+                return 0;
+            }
+
+            cout << "el anio " << anio << " no es bisiesto.";
+            return 1;
+        }
+
+        cout << "el anio " << anio << " es bisiesto.";
+        return 0;
+    }
+
+    cout << "el anio " << anio << " no es bisiesto.";
+    return 1;
+};
